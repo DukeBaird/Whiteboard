3c5b05541df3da38bb359824181f5859192cae6e
Ben Craig <benjamin.d.craig@gmail.com>
2015-05-19 09:08:42 -0400

    removed unnecesasry class tag

--- router.js
+++ router.js
@@ -1,68 +1,68 @@
 var url = require("url");
-var MongoClient = require('mongodb').MongoClient;
+// var MongoClient = require('mongodb').MongoClient;
 
 
 handle = {};
 
 function route(req, res) {
 
-  var pathname = url.parse(req.url).pathname;
-  var path = "/" + req.params.page;
+    var pathname = url.parse(req.url).pathname;
+    var path = "/" + req.params.page;
 
-  // console.log(path);
-  // console.log("Routing to " + pathname);
-
-
-  if (typeof (handle[path]) === 'function') {
-    handle[path](req, res, pathname);
-  } else if (pathname === '/') {
+    // console.log(path);
+    // console.log("Routing to " + pathname);
 
 
-
-    // console.log("testing mongodb");
-    // MongoClient.connect("mongodb://localhost:27017/testdb", function(err, db) {
-    //   if (!err) {
-    //     console.log("connected!");
-
-    //     var collection = db.collection('test');
-
-    //     collection.insert({
-    //       'hello': 'test'
-    //     }, function(err, result) {
-    //         if (err) {
-    //           console.log(err);
-    //         } else {
-    //           console.log(result);
-    //           console.log('no err');
-
-    //           collection.find({
-    //             'hello': 'test'
-    //           }).toArray(function(err, item) {
-    //             if (err) {
-    //               console.log(err);
-    //             } else {
-    //               console.log(item);
-    //             }
-    //           });
-    //         }
-    //       });
-
-    //   } else {
-    //     console.log(err);
-    //   }
-    // });
+    if (typeof (handle[path]) === 'function') {
+        handle[path](req, res, pathname);
+    } else if (pathname === '/') {
 
 
 
+        // console.log("testing mongodb");
+        // MongoClient.connect("mongodb://localhost:27017/testdb", function(err, db) {
+        //   if (!err) {
+        //     console.log("connected!");
 
-    res.render('index', {
-      pageTitle: "Home"
-    });
-  } else {
-    res.render('404', {
-      pageTitle: "404 - Page Not Found"
-    });
-  }
+        //     var collection = db.collection('test');
+
+        //     collection.insert({
+        //       'hello': 'test'
+        //     }, function(err, result) {
+        //         if (err) {
+        //           console.log(err);
+        //         } else {
+        //           console.log(result);
+        //           console.log('no err');
+
+        //           collection.find({
+        //             'hello': 'test'
+        //           }).toArray(function(err, item) {
+        //             if (err) {
+        //               console.log(err);
+        //             } else {
+        //               console.log(item);
+        //             }
+        //           });
+        //         }
+        //       });
+
+        //   } else {
+        //     console.log(err);
+        //   }
+        // });
+
+
+
+
+        res.render('index', {
+            pageTitle: "Home"
+        });
+    } else {
+        res.render('404', {
+            pageTitle: "404 - Page Not Found"
+        });
+    }
 }
 
 exports.route = route;
