f5e3a14bd481b252c95bc621c870a79fbfd77320
Ben Craig <benjamin.d.craig@gmail.com>
2015-06-11 15:17:20 -0400

    mousedown and mouseup coords as JSON and broadcasted

--- public/js/viewJS/index.js
+++ public/js/viewJS/index.js
@@ -4,18 +4,31 @@ var c = document.getElementById('whiteboard');
 var ctx = c.getContext('2d');
 var socket = io();
 var shape = 'circle';
+var initialCoord = {
+	x: 0,
+	y: 0
+}
+var finalCoord = {
+	x: 0,
+	y: 0
+}
 
-c.addEventListener('click', function (e) {
+c.addEventListener('mousedown', function (e) {
+	// console.log(e);
+	initialCoord.x = e.x;
+	initialCoord.y = e.y;
+});
+
+c.addEventListener('mouseup', function (e) {
+	// console.log(e);
+
+	finalCoord.x = e.x;
+	finalCoord.y = e.y;
+
 	var params = {
 		shape: shape,
-		initialCoord: {
-			x: 55,
-			y: 55
-		},
-		finalCoord: {
-			x: 66,
-			y: 66
-		}
+		initialCoord: initialCoord,
+		finalCoord: finalCoord
 	};
 	
 	socket.emit('draw', params);
