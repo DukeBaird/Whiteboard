2745bcf86a8a7590fed7ccb13b3403706bf9a52c
Ben Craig <benjamin.d.craig@gmail.com>
2015-06-14 00:07:16 -0400

    marker + clear canvas

--- public/js/viewJS/index.js
+++ public/js/viewJS/index.js
@@ -4,6 +4,7 @@ var c = document.getElementById('whiteboard');
 var ctx = c.getContext('2d');
 var socket = io();
 var shape = 'circle';
+var isMouseDown = false;
 var initialCoord = {
 	x: 0,
 	y: 0
@@ -13,14 +14,31 @@ var finalCoord = {
 	y: 0
 }
 
+c.addEventListener('mousemove', function (e) {
+	if (isMouseDown && shape === 'marker') {
+
+	initialCoord.x = e.x;
+	initialCoord.y = e.y;
+
+	var params = {
+		shape: shape,
+		initialCoord: initialCoord
+	};
+
+		socket.emit('draw', params);
+	}
+});
+
 c.addEventListener('mousedown', function (e) {
 
+	isMouseDown = true;
 	initialCoord.x = e.x;
 	initialCoord.y = e.y;
 });
 
 c.addEventListener('mouseup', function (e) {
 
+	isMouseDown = false;
 	finalCoord.x = e.x;
 	finalCoord.y = e.y;
 
@@ -37,6 +55,7 @@ document.getElementById('circleButton').addEventListener('click', function (e) {
 	document.getElementById('circleButton').className = 'clicked';
 	document.getElementById('lineButton').className = '';
 	document.getElementById('squareButton').className = '';
+	document.getElementById('markerButton').className = '';
 	shape = 'circle';
 	// console.log('Clicked circle button');
 });
@@ -45,6 +64,7 @@ document.getElementById('lineButton').addEventListener('click', function (e) {
 	document.getElementById('circleButton').className = '';
 	document.getElementById('lineButton').className = 'clicked';
 	document.getElementById('squareButton').className = '';
+	document.getElementById('markerButton').className = '';
 	shape = 'line';
 	// console.log('Clicked line button');
 });
@@ -53,12 +73,23 @@ document.getElementById('squareButton').addEventListener('click', function (e) {
 	document.getElementById('circleButton').className = '';
 	document.getElementById('lineButton').className = '';
 	document.getElementById('squareButton').className = 'clicked';
-	shape ='square';
+	document.getElementById('markerButton').className = '';
+	shape = 'square';
 	// console.log('Clicked square button');
+});
+
+document.getElementById('markerButton').addEventListener('click', function (e) {
+	document.getElementById('circleButton').className = '';
+	document.getElementById('lineButton').className = '';
+	document.getElementById('squareButton').className = '';
+	document.getElementById('markerButton').className = 'clicked';
+	shape = 'marker';
+
 });
 
 document.getElementById('clear').addEventListener('click', function (e) {
 	console.log('Clear Canvas');
+	ctx.clearRect(0, 0, c.width, c.height);
 });
 
 socket.on('draw', function (params) {
@@ -82,6 +113,15 @@ socket.on('draw', function (params) {
 			Math.sqrt(Math.pow(params.finalCoord.x - params.initialCoord.x, 2) + Math.pow(params.finalCoord.y - params.initialCoord.y, 2)),
 			0, 2 * Math.PI, false);
 		ctx.stroke();
-	}
+	} else if (params.shape === 'marker') {
+		ctx.strokeStyle = 'green';
+		ctx.beginPath();
+		ctx.arc(params.initialCoord.x, params.initialCoord.y,
+			5,
+			0, 2 * Math.PI, false);
+		ctx.fillStyle = 'green';
+		ctx.fill();
+		ctx.stroke();
+	} 
 
 });
